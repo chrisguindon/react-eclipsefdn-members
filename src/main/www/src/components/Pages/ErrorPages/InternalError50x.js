dbd717dea6cc28cd597c757f938f04491c35000a
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2021-06-30 14:30:56 -0400

    fix indent
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- src/main/www/src/components/Pages/ErrorPages/InternalError50x.js
+++ src/main/www/src/components/Pages/ErrorPages/InternalError50x.js
@@ -4,6 +4,6 @@ export default function InternalError50x() {
       <h1>An unexpected error occurred</h1>
       <p>Sorry, the page you are looking for is currently unavailable.</p>
       <p>Please try again later.</p>
-   </>
+    </>
   );
 }
